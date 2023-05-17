fd516a8343699a044f8b6da630eee6cab293db71
Eric Ross <eric.ross@@hp.com>
2023-05-16 19:40:57 -0700

    Python command to process passed in file

---
+++ bondcalc.py
@@ -0,0 +1,38 @@
+# %%
+import pandas as pd
+from pprint import pprint as pp
+import sys
+import tempfile
+# %%
+file = sys.argv[1]
+tfile = tempfile.NamedTemporaryFile().name
+# %%
+with open(file, 'r') as fin:
+    data = fin.read().splitlines(True)
+
+data = data[1:]
+data = data[:-1]
+
+with open(tfile, 'w') as fout:
+    fout.writelines(data)
+
+# %%
+dfira = pd.read_csv(tfile)
+
+# %%
+dfira['Gains']=dfira['Quantity'].replace('[\$,]', '', regex=True).astype(float)+(dfira['Amount'].replace('[\$,]', '', regex=True).astype(float))
+
+# %%
+dfira['TDate']=dfira['Description'].str.extract(r'(US TREASURY BILL23U S T BILL DUE )(.*)')[1]
+
+# ira account gains
+dfbuyira=dfira.query('Description.str.contains("US TREASURY") and Action=="Buy" and not TDate.isnull()')
+pp(dfbuyira, width=800)
+
+# %%
+# ira total gains
+print("%.2f" % dfbuyira['Gains'].sum())
+
+
+
+# %%
